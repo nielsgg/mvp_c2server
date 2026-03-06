2651e7bed395cbb059309d7e085ea32c775a39dc
Simon Funke <simonfunkec@gmail.com>
2026-03-06 14:37:13 +0100

    Nmap Parser

--- src/clientDataManger.ts
+++ src/clientDataManger.ts
@@ -8,6 +8,60 @@ export type ClientData = {
     user: string;
     lastSeen: number; // Timestamp for last seen.
 };
+
+type NmapService = {
+  name?: string;
+  product?: string;
+  version?: string;
+  extrainfo?: string;
+};
+
+type NmapPort = {
+  portid: number | string;
+  protocol: string;
+  state?: string | { state: string };
+  service?: NmapService;
+};
+
+type NmapHost = {
+  addresses?: {
+    ipv4?: string;
+    ipv6?: string;
+    mac?: string;
+  };
+  hostnames?: { name?: string; type?: string }[];
+  ports?: NmapPort[];
+  os?: {
+    name?: string;
+    accuracy?: string;
+    osfamily?: string;
+    osgen?: string;
+  };
+  traceroute?: {
+    ttl?: number;
+    ip?: string;
+    rtt?: string;
+  }[];
+};
+
+type NmapScanEntry = {
+  host?: NmapHost;
+};
+
+type ParsedMachine = {
+  ip: string;
+  hostname?: string;
+  os?: string;
+  openPorts: {
+    port: number;
+    protocol: string;
+    serviceName?: string;
+    product?: string;
+    version?: string;
+  }[];
+  hops: string[];
+};
+
 export type ClientTable = ProbingHashtable<number, ClientData>;
 
 /**
